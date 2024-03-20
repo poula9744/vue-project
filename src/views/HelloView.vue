73ead0912b1bdf993e3159f59a6763b5f81e0023
poula9744 <poula9744@naver.com>
2024-03-20 16:09:04 +0900

    2024-03-20

---
+++ src/views/HelloView.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <h1> Hello Vue.js</h1>
+
+</template>
+
+<script>
+export default{
+    name: "HelloView",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style></style>
